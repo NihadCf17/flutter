6b014aaa85111dd0a2d7a59b2506e66091e76de0
NihadCf17 <nihadiko7@gmail.com>
2025-02-17 20:31:03 +0400

    teze

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,24 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               gradient: LinearGradient(
                 colors: [
                    Color.fromARGB(255, 24, 211, 193),
                    Color.fromARGB(255, 9, 211, 73),
             ],
+            begin: Alignment.topLeft, 
+            end: Alignment.bottomRight
           )),
           child: const Center(
-            child: Text('Hello World'),
+            child: Text('Hello World',
+            style: TextStyle(color:Colors.purpleAccent,
+            fontSize: 40
+            
+            )
+            
+            
+            )
           ),
         ),
       ),
