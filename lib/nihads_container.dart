f600025766d8cea33058818ebc0d16ffc7e20585
NihadCf17 <nihadiko7@gmail.com>
2025-07-03 16:13:55 +0400

    a

--- lib/nihads_container.dart
+++ lib/nihads_container.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_teze/styled_text.dart';
 
+class NihadsContainer extends StatelessWidget {
+  const  NihadsContainer({super.key});
+@override
+Widget build(context) {
+  return  Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                   Color.fromARGB(255, 24, 189, 112),
+                   Color.fromARGB(255, 255, 255, 255),
+            ],
+            begin: Alignment.topLeft, 
+            end: Alignment.bottomRight
+          )),
+          child: const Center(
+            child: StyledText()
+          ),
+        ) ;
+}
+}
